64b44420dc89b349b41642304d4a0e2f3d229517
Marcelo Vani <marcellovani@yahoo.co.uk>
2017-07-25 12:13:46 +0100

    Adding classes

---
+++ src/Behat/TermManagerExtension/Context/TermManagerContext.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Behat\TermManagerExtension\Context;
+
+use Behat\Behat\Hook\Scope\BeforeStepScope;
+use Behat\MinkExtension\Context\RawMinkContext;
+
+/**
+ * Class TermManagerContext
+ *
+ * @package Behat\TermManagerExtension\Context
+ */
+class TermManagerContext extends RawMinkContext implements TermManagerInterface
+{
+
+}
---
+++ src/Behat/TermManagerExtension/Context/TermManagerInterface.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Behat\TermManagerExtension\Context;
+
+use Behat\Behat\Context\Context;
+
+interface TermManagerInterface extends Context
+{
+
+}
